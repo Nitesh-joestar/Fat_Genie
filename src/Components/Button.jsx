68d846f3cca360441230f79b5a73aae43008e408
NItesh KUmar S <120151860+Nitesh-joestar@users.noreply.github.com>
2025-07-10 10:07:08 +0530

    tt

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Buttons(){
     return(
            <button className="px-4 py-2 rounded-md border border-neutral-300 bg-neutral-100 text-neutral-500 text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
-        Simple gg
+        Simple see if its working
       </button>
 
     );
